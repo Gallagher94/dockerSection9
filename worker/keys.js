e10b483b723786ae3a8963281c5c476ad4e272ab
Gallagher94 <ruairidhgallaghers3@gmail.com>
2019-02-16 16:18:31 +0000

    Redirect to ui from nginx not working

---
+++ worker/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT
+};
+// export default {
+//   redisHost: process.env.REDIS_HOST,
+//   redisPort: process.env.REDIS_PORT
+// };
